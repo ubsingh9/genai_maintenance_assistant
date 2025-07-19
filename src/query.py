9e157474a23468e90e88ebde013bcd5d63ee0fbd
ubsingh9 <icfai.uday@gmail.com>
2025-07-19 14:41:54 +0530

    api key change

--- src/query.py
+++ src/query.py
@@ -35,7 +35,7 @@ def get_qa_chain():
         input_variables=['context', 'questions']
     )
 
-    llm=ChatGroq(groq_api_key=groq_api_key, model="llama-3.1-8b-instant", temperature=0.2)
+    llm=ChatGroq(groq_api_key=GROQ_API_KEY, model="llama-3.1-8b-instant", temperature=0.2)
     qa_chain = RetrievalQA.from_chain_type(
         llm=llm,
         retriever=retriever,
